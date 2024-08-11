23324be3248a700f02dafb665c7db9e19dcc22c2
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-08-11 15:27:32 -0300

    Questão 5
    Crie um programa que exiba os números de 1 a 10 usando um loop for.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,4 @@
-﻿Console.WriteLine("Digite um número:");
-int number = int.Parse(Console.ReadLine());
-
-if (number > 0){
-    Console.WriteLine($"{number} é positivo");
-}
-else if( number < 0){
-    Console.WriteLine($"{number} é negativo");
-}
-else{
-    Console.WriteLine($"{number} é zero");
+﻿for (int i = 1; i < 11; i++) 
+{
+  Console.WriteLine(i);
 }

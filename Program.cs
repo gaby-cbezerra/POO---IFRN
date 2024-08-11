82c0fc4adaa1dc30c3f487d5ac9dbd057cea1165
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-08-11 15:01:06 -0300

    Questão 1
    Lista 1 - Escreva um programa em C# que declare variáveis dos tipos int, float, string e bool. Atribua valores a essas variáveis e imprima-os no console.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int number1 = 19;
+float number2 = 9.5f;
+string word = "Olá";
+bool number3 = true;
+
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+Console.WriteLine(word);
+Console.WriteLine(number3);

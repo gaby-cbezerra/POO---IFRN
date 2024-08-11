a1d66cd0125b2c24c9f19f6f84b0d63cc899808f
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-08-11 20:33:59 -0300

    Questão 9
    Crie um programa que solicite números ao usuário até que ele digite zero. Ao final, o programa deve exibir a soma de todos os números digitados.

--- Program.cs
+++ Program.cs
@@ -1,39 +1,13 @@
-﻿        Console.Write("Digite o primeiro número: ");
-        int num1 = int.Parse(Console.ReadLine());
-        Console.Write("Digite o segundo número: ");
-        int num2 = int.Parse(Console.ReadLine());
-
-        Console.Write("Digite o terceiro número: ");
-        int num3 = int.Parse(Console.ReadLine());
-
-        int maior;
-        if (num1 >= num2 && num1 >= num3)
-        {
-            maior = num1;
-        }
-        else if (num2 >= num1 && num2 >= num3)
-        {
-            maior = num2;
-        }
-        else
-        {
-            maior = num3;
-        }
-
-        int menor;
-        if (num1 <= num2 && num1 <= num3)
-        {
-            menor = num1;
-        }
-        else if (num2 <= num1 && num2 <= num3)
-        {
-            menor = num2;
-        }
-        else
-        {
-            menor = num3;
-        }
-
-        Console.WriteLine($"O maior número é: {maior}");
-        Console.WriteLine($"O menor número é: {menor}");
+﻿int soma = 0;
+int number;
+do {
+    Console.WriteLine("Digite um número:");
+    number = int.Parse(Console.ReadLine());
     
+    if ( number != 0){
+    soma += number;
+    }
+
+}  while( number != 0);
+
+Console.WriteLine($"A soma de todos os números digitados é: {soma}");

81dae3eda3830e8cea36841d050418d64061cba3
gaby-cbezerra <135763397+gaby-cbezerra@users.noreply.github.com>
2024-08-12 08:58:01 -0300

    Questão 11
    Implemente uma função recursiva em C# chamada CalcularFatorial que recebe um número inteiro n e retorna o fatorial dele. Utilize operadores lógicos para garantir que a função trate adequadamente os casos de entrada negativa e zero. Teste a função a partir do método Main.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,39 @@
-﻿Console.WriteLine("Informe um valor em R$ para conversão em dólar:");
-float money = float.Parse(Console.ReadLine());
-Console.WriteLine("Informe a taxa de câmbio:");
-float taxa = float.Parse(Console.ReadLine());
+﻿using System;
 
-float dol = money * taxa;
+class Program
+{
+    
+    static int CalcularFatorial(int n)
+    {
+        if (n < 0)
+        {
+            throw new ArgumentException("O número deve ser não-negativo.");
+        }
 
-Console.WriteLine($"O valor é {dol}$");
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return n * CalcularFatorial(n - 1);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Fatorial de 4: " + CalcularFatorial(4));  
+            Console.WriteLine("Fatorial de 0: " + CalcularFatorial(0));  
+            Console.WriteLine("Fatorial de 5: " + CalcularFatorial(5));  
+            Console.WriteLine("Fatorial de -3: " + CalcularFatorial(-3)); 
+        }
+         catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+}
+}
+
+    
